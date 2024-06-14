2c2ca44d15a3c6384ed597aea86545fd3dcea556
LucidMedussa <nwunelilucychiamaka@gmail.com>
2024-06-13 22:09:26 -0700

    Made changes to tf files

--- scripts/jenkins_install.sh
+++ scripts/jenkins_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo yum -y update
 
-sudo yum -y install java-21-amazon-*
+sudo yum -y install java-21-amazon-correto
 
 sudo yum -y install git
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repoLinks 
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo 
 
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.keyLinks 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key 
 
 sudo yum -y install jenkins
 sudo fallocate -l 1G /swapfile_extend_1GB
--- security_group.tf
+++ security_group.tf
@@ -18,17 +18,16 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
+   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
