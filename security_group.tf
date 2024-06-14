ae0365b6fc8c8ea10754bb3da413f30b90a2d4c8
LucidMedussa <nwunelilucychiamaka@gmail.com>
2024-06-13 23:13:40 -0700

    Made changes to tf files

--- security_group.tf
+++ security_group.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "web_sg" {
   }
   
    ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
